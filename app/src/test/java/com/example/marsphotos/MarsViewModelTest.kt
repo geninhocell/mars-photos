6bc4866c5da27441165e001a6dd958130a329f18
geninhocell <geninhocell@gmail.com>
2024-02-16 17:37:58 -0300

    added: retrofit and coil

--- app/src/test/java/com/example/marsphotos/MarsViewModelTest.kt
+++ app/src/test/java/com/example/marsphotos/MarsViewModelTest.kt
@@ -1,12 +1,18 @@
-package com.example.marsphotos.fake
+package com.example.marsphotos
 
+import com.example.marsphotos.fake.FakeDataSource
+import com.example.marsphotos.fake.FakeNetworkMarsPhotosRepository
+import com.example.marsphotos.rules.TestDispatcherRule
 import com.example.marsphotos.ui.screens.MarsUiState
 import com.example.marsphotos.ui.screens.MarsViewModel
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 
 class MarsViewModelTest {
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
     @Test
     fun marsViewModel_getMarsPhotos_verifyMarsUiStateSuccess() = runTest{
         val marsViewModel = MarsViewModel(
@@ -14,8 +20,7 @@ class MarsViewModelTest {
         )
 
         assertEquals(
-            MarsUiState.Success("Success: ${FakeDataSource.photosList.size} Mars " +
-                    "photos retrieved"),
+            MarsUiState.Success(FakeDataSource.photosList),
             marsViewModel.marsUiState
         )
     }
--- app/src/test/java/com/example/marsphotos/NetworkMarsRepositoryTest.kt
+++ app/src/test/java/com/example/marsphotos/NetworkMarsRepositoryTest.kt
@@ -1,6 +1,8 @@
-package com.example.marsphotos.fake
+package com.example.marsphotos
 
 import com.example.marsphotos.data.NetworkMarsPhotosRepository
+import com.example.marsphotos.fake.FakeDataSource
+import com.example.marsphotos.fake.FakeMarsApiService
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
